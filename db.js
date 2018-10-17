bf3b775c14df69bfa0f410802be20fd421699089
Justin Weiskittel <justinweiskittel@gmail.com>
2018-10-17 17:53:13 -0400

    commit again

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.enc.PASS)}@localhost/${process.env.NAME}`, {
     dialect: 'postgres'
 })
 
